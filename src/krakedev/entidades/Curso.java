79ca81ffa95a3fbe585352c5867058d707650b1a
WilsonG08 <wilson.guayanay@epn.edu.ec>
2023-12-16 12:17:36 -0500

    Ejercicio notas

---
+++ src/krakedev/entidades/Curso.java
@@ -0,0 +1,107 @@
+package krakedev.entidades;
+
+import java.util.ArrayList;
+
+public class Curso {
+	private ArrayList<Estudiante> estudiantes;
+
+	public Curso() {
+		estudiantes = new ArrayList<Estudiante>();
+	}
+
+	public Curso(ArrayList<Estudiante> estudiantes) {
+		this.estudiantes = estudiantes;
+	}
+
+	public ArrayList<Estudiante> getEstudiantes() {
+		return estudiantes;
+	}
+
+	public void setEstudiantes(ArrayList<Estudiante> estudiantes) {
+		this.estudiantes = estudiantes;
+	}
+
+	public String buscarEstudiantePorCedula(Estudiante est) {
+
+		String cedulaBuscada = est.getCedula();
+
+		for (int i = 0; i < estudiantes.size(); i++) {
+			Estudiante estudiante = estudiantes.get(i);
+			if (estudiante.getCedula().equals(cedulaBuscada)) {
+				System.out.println("Ya esta inscrito en el curso");
+			}
+		}
+		return null;
+	}
+
+	public void matricularEstudiante(Estudiante estudiante) {
+		String mensaje = buscarEstudiantePorCedula(estudiante);
+
+		if (mensaje == null) {
+			estudiantes.add(estudiante);
+			System.out.println("Estudiante matriculado con éxito.");
+		} else {
+			System.out.println(mensaje);
+		}
+	}
+
+	public double calcularPromedioCurso() {
+		double sumaPromedios = 0;
+		int totalEstudiantes = estudiantes.size();
+
+		if (totalEstudiantes > 0) {
+			for (int i = 0; i < totalEstudiantes; i++) {
+				Estudiante estudiante = estudiantes.get(i);
+				ArrayList<Nota> notasEstudiante = estudiante.getNotas();
+				double sumaNotas = 0;
+				int totalMaterias = notasEstudiante.size();
+
+				if (totalMaterias > 0) {
+					for (int j = 0; j < totalMaterias; j++) {
+						Nota nota = notasEstudiante.get(j);
+						sumaNotas += nota.getCalificacion();
+					}
+					double promedioEstudiante = sumaNotas / totalMaterias;
+					sumaPromedios += promedioEstudiante;
+				}
+			}
+
+			return sumaPromedios / totalEstudiantes;
+		} else {
+			return 0.0;
+		}
+	}
+
+	public void mostrar() {
+		System.out.print("Curso [estudiantes=[");
+		int totalEstudiantes = estudiantes.size();
+
+		for (int i = 0; i < totalEstudiantes; i++) {
+			Estudiante estudiante = estudiantes.get(i);
+			System.out.print("Estudiante [nombre=" + estudiante.getNombre() + ", apellido=" + estudiante.getApellido()
+					+ ", cedula=" + estudiante.getCedula() + ", notas=[");
+
+			ArrayList<Nota> notasEstudiante = estudiante.getNotas();
+			int totalNotas = notasEstudiante.size();
+
+			for (int j = 0; j < totalNotas; j++) {
+				Nota nota = notasEstudiante.get(j);
+				System.out.print("Materia [codigo=" + nota.getMateria().getCodigo() + ", nombre="
+						+ nota.getMateria().getNombre() + ", calificacion=" + nota.getCalificacion() + "]");
+
+				if (j < totalNotas - 1) {
+					System.out.print(", ");
+				}
+			}
+
+			System.out.print("]");
+
+			if (i < totalEstudiantes - 1) {
+				System.out.print(", ");
+			}
+		}
+
+		System.out.println("]]");
+	}
+
+}
---
+++ src/krakedev/entidades/Estudiante.java
@@ -0,0 +1,127 @@
+package krakedev.entidades;
+
+import java.util.ArrayList;
+
+public class Estudiante {
+	private String cedula;
+	private String nombre;
+	private String apellido;
+
+	private ArrayList<Nota> notas;
+
+	public Estudiante(String cedula, String nombre, String apellido) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		notas = new ArrayList<Nota>();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public ArrayList<Nota> getNotas() {
+		return notas;
+	}
+
+	public void setNotas(ArrayList<Nota> notas) {
+		this.notas = notas;
+	}
+
+	public void agregarNota(Nota nuevaNota) {
+		double calificacion = nuevaNota.getCalificacion();
+		String codigoMateria = nuevaNota.getMateria().getCodigo();
+
+		if (calificacion >= 0 && calificacion <= 10) {
+			boolean notaExistente = false;
+
+			for (int i = 0; i < notas.size(); i++) {
+				Nota nota = notas.get(i);
+				if (nota.getMateria().getCodigo().equals(codigoMateria)) {
+					notaExistente = true;
+					break;
+				}
+			}
+
+			if (!notaExistente) {
+				notas.add(nuevaNota);
+				System.out.println("Nueva nota añadida correctamente");
+			} else {
+				System.out.println("Ya existe una nota con el codigo " + codigoMateria + ".");
+			}
+		} else {
+			System.out.println("Error : La nueva nota debe estar entre 0 y 10.");
+		}
+	}
+
+	public void modificarNota(String codigoMateria, double nuevaNota) {
+		if (nuevaNota >= 0 && nuevaNota <= 10) {
+			boolean materiaEncontrada = false;
+
+			for (int i = 0; i < notas.size(); i++) {
+				Nota nota = notas.get(i);
+				if (nota.getMateria().getCodigo().equals(codigoMateria)) {
+					nota.setCalificacion(nuevaNota);
+					materiaEncontrada = true;
+					System.out
+							.println("Calificación modificada con éxito para la materia con código: " + codigoMateria);
+					break;
+				}
+			}
+
+			if (!materiaEncontrada) {
+				System.out.println("No se encontró una materia con el código " + codigoMateria + ".");
+			}
+		} else {
+			System.out.println("La nueva calificación debe estar entre 0 y 10.");
+		}
+	}
+
+	public double calcularPromedioNotasEstudiante() {
+		double sumaNotas = 0.0;
+		int totalMaterias = notas.size();
+
+		if (totalMaterias == 0) {
+			System.out.println("El estudiante " + nombre + " " + apellido
+					+ " no tiene calificaciones registradas. El promedio es 0.");
+			return 0.0;
+		}
+
+		for (int i = 0; i < notas.size(); i++) {
+			Nota nota = notas.get(i);
+			sumaNotas += nota.getCalificacion();
+		}
+		double promedio = sumaNotas / totalMaterias;
+		return promedio;
+	}
+
+	public void mostrar() {
+		String infoEstudiante = "Estudiante [cedula=" + cedula + ",nombre=" + nombre + ",apellido=" + apellido + "]";
+		System.out.println(infoEstudiante);
+		for (int i = 0; i < notas.size(); i++) {
+			String infoNota = "Nota [materia=" + notas.get(i).getMateria().getNombre() + ", calificacion="
+					+ notas.get(i).getCalificacion() + "]";
+			System.out.println(infoNota);
+		}
+	}
+}
